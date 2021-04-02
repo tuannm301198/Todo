2aebe9ae2140f50bbdc9d72ccb65ea04356b39ca
Admin <admin@Admins-Mac-mini.local>
2021-04-02 14:56:01 +0700

    test umi

--- src/pages/Store/components/ItemList.tsx
+++ src/pages/Store/components/ItemList.tsx
@@ -10,54 +10,17 @@ const api = 'https://reqres.in/api/product';
 // list of item
 export const ItemList = (props: any) => {
   const { addToCart } = props;
-  const { error, loading } = useRequest(getList, { onSuccess: (result) => setList(result) });
-  const [list, setList] = useState([]);
   const [paging, setPaging] = useState(1);
-  const [scrollPosition, setScrollPosition] = useState(0);
-  useEffect(() => {
-    getList();
-    console.log(navigator)
-    window.addEventListener('scroll', handleScroll, { passive: true });
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
-  useEffect( () => {
-     request(api, {
-      params: {
-        page: paging,
-      },  
-    })
-      .then((res) => {
-        setList(list.concat(res.data));
-      })
-      .catch((err) => alert(err));
-  }, [paging]);
-  
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
-    console.log(position);
-  };
-
-  function getList() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(
-          request(api, {
-            params: {
-              page: paging,
-            },
-          })
-            .then((res) => {
-              return res.data;
-            })
-            .catch((err) => alert(err)),
-        );
-      }, 1000);
-    });
-  }
+  const { data, error, loading } = useRequest({
+    url: api,
+    params: {
+      page: paging,
+    },
+  },{
+    refreshDeps: [paging]
+  });
+ 
   if (error) {
     return <div>Can't process</div>;
   }
@@ -73,7 +36,7 @@ export const ItemList = (props: any) => {
   return (
     <div>
       <Button onClick={() => setPaging(paging + 1)}>Load more</Button>
-      <Table dataSource={list} rowKey={(key) => key.id} bordered>
+      <Table dataSource={data.data} rowKey={(key) => key.id} bordered>
         <Column title="Id" dataIndex="id" responsive={['sm']} />
         <Column title="Name" dataIndex="name" responsive={['md']} className="cap" />
         <Column title="Price" dataIndex="year" responsive={['sm']} />
