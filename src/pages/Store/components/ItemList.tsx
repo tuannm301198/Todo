fa94c81cbde899e5f7a2fba147d6f0cead5d30cc
Admin <admin@Admins-Mac-mini.local>
2021-04-02 17:15:20 +0700

    test function

--- src/pages/Store/components/ItemList.tsx
+++ src/pages/Store/components/ItemList.tsx
@@ -1,15 +1,12 @@
 import '../styles.css';
+import request from 'umi-request'
 import { useState } from 'react';
 import { useRequest } from '@umijs/hooks';
 import { Table, Button, Spin } from 'antd';
 
 const { Column } = Table;
 const api = 'https://reqres.in/api/product';
-const list = [{
-  id:69,
-  name: 'boring',
-  year: 2000,
-}]
+
 // list of item
 export const ItemList = (props: any) => {
   const { addToCart } = props;
@@ -20,32 +17,30 @@ export const ItemList = (props: any) => {
       page: paging,
     },
   },{
-    refreshDeps: [paging],
     formatResult: (res) => {
       return res.data
     }
     
   });
-  
-  // async function loadMoreData() {
-  //   setPaging(paging + 1)
-  //   const newData = [...data]
-  //   return new Promise((resolve) => {
-  //     setTimeout(() => {
-  //       resolve(
-  //         request(api,
-  //           {
-  //             params: {
-  //               page: paging,
-  //             }
-  //           })
-  //           .then((res) =>{
-  //             return data.data.concat(res.data)
-  //           })
-  //       );
-  //     }, 1000);
-  //   });
-  // }
+  const [list,setList] = useState(data)
+  function loadMoreData() {
+    setPaging(paging + 1)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(
+          request(api,
+            {
+              params: {
+                page: paging,
+              }
+            })
+            .then((res) =>{
+              setList(data.concat(res.data))
+            })
+        );
+      }, 1000);
+    });
+  }
   if (error) {
     return <div>Can't process</div>;
   }
@@ -57,11 +52,11 @@ export const ItemList = (props: any) => {
       </div>
     );
   }
-
+  
   return (
     <div>
-      <Button onClick={() => setPaging(paging + 1)}>Load more</Button>
-      <Table dataSource={data && data} rowKey={(key) => key.id} bordered>
+      <Button onClick={() => loadMoreData()}>Load more</Button>
+      <Table dataSource={list ?? data} rowKey={(key) => key.id} bordered>
         <Column title="Id" dataIndex="id" responsive={['sm']} />
         <Column title="Name" dataIndex="name" responsive={['md']} className="cap" />
         <Column title="Price" dataIndex="year" responsive={['sm']} />
