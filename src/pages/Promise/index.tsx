5df5fa5cebe0852e10a205a4992a73d167354e96
Admin <admin@Admins-Mac-mini.local>
2021-04-13 11:28:21 +0700

    reducer

--- src/pages/Promise/index.tsx
+++ src/pages/Promise/index.tsx
@@ -1,80 +1,84 @@
-import { useState, useEffect } from 'react';
-import { Pagination, Button, Select } from 'antd';
+import { useEffect, useReducer } from 'react';
+import { Button } from 'antd';
 import request from 'umi-request';
 
-const {Option} = Select
+const initState = {
+  data: [],
+  dataLength: 0,
+  requestPage: 1,
+  apiDataLength: 0,
+};
+const addMoreReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MORE':
+      return addMore(state, action.payload);
+    case 'FETCH_DATA':
+      return fetchData(state, action.payload);
+    default:
+      return state;
+  }
+};
+const addMore = (state, data) => {
+  return {
+    ...state,
+    requestPage: data.requestPage + 1,
+  };
+};
+const fetchData = (state, data) => {
+  console.log(state);
+
+  const newData = [...state.data];
+  console.log(newData);
+
+  return {
+    ...state,
+    data: newData.concat(data.data),
+    requestPage: data.page,
+    apiDataLength: data.total,
+    dataLength: data.per_page,
+  };
+  // return newData.data.concat(...state.data);
+};
 const api = 'https://reqres.in/api/users';
 const Promise = () => {
-  const [data, setData] = useState([]);
-  const [dataLength, setDataLength] = useState(0);
-  const [perPage, setPerPage] = useState(2);
-  const [minPerPage, setMinPerPage] = useState(0);
-  const [maxPerPage, setMaxPerPage] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [requestPage, setRequestPage] = useState(1);
-  const [apiDataLength, setApiDataLength] = useState(0)
+  const [state, dispatch] = useReducer(addMoreReducer, initState);
   useEffect(async () => {
     await request(api, {
       params: {
-        page: requestPage,
+        page: state?.requestPage || 1,
       },
-    }).then((res) => {
-      setData(prev => prev.concat(res.data));
-      
-      setDataLength(prev => prev + res.data.length);
-      setMaxPerPage(perPage);
-      setApiDataLength(res.total)
-      
-    });
-  }, [requestPage]);
-  useEffect(() => {
-    setMaxPerPage(perPage)
-  },[perPage])
+    })
+      .then((res) => {
+        fetchData(res);
+      })
+      .catch((err) => alert(err));
+  }, [state.requestPage]);
+  console.log(state);
 
-  const onChange = (page: any) => {
-    setCurrentPage(page);
-    setMaxPerPage(page * perPage);
-    setMinPerPage((page - 1) * perPage);
+  const fetchData = (product) => {
+    dispatch({ type: 'FETCH_DATA', payload: product });
+  };
+
+  const addMore = (product) => {
+    dispatch({ type: 'ADD_MORE', payload: product });
   };
 
   return (
     <div>
-      <Button disabled={dataLength >= apiDataLength}
+      <Button
+        disabled={state.dataLength >= state.apiDataLength}
         onClick={() => {
-          setRequestPage(requestPage + 1);
+          addMore(state);
         }}
       >
         Load more
       </Button>
-      <Select
-        defaultValue={2}
-        onChange={(e) => {
-          setPerPage(e);
-        }}
-      >
-        <Option value={2}>2</Option>
-        <Option value={3}>3</Option>
-        <Option value={4}>4</Option>
-        <Option value={5}>5</Option>
-        <Option value={6}>6</Option>
-      </Select>
-      {data.map(
-        (item: any, idx) =>
-          idx >= minPerPage &&
-          idx < maxPerPage && (
-            <div key={item.id}>
-              <div>{item.email}</div>
-            </div>
-          ),
-      )}
-      <Pagination
-        current={currentPage}
-        onChange={onChange}
-        total={dataLength}
-        defaultPageSize={perPage}
-        pageSize={perPage}
-        showTotal={(total) => `Total of ${total} items`}
-      />
+
+      {state?.data?.map((item: any) => (
+        <div key={item.id}>
+          <div>{item.email}</div>
+        </div>
+      ))}
     </div>
   );
 };
